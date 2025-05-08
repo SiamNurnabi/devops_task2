0c9b3bab85ec5a1c23ed62bf34c90088f608bd70
SiamNurnabi <siam.nurnabi@dsinnovators.com>
2025-05-08 11:37:08 +0600

    Dependency added for git and showing commit hash

--- src/main/java/com/example/devops_task2/controller/ViewController.java
+++ src/main/java/com/example/devops_task2/controller/ViewController.java
@@ -1,5 +1,6 @@
 package com.example.devops_task2.controller;
 
+import org.springframework.boot.info.GitProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ViewController {
 
+    private final GitProperties gitProperties;
+
+    public ViewController(GitProperties gitProperties) {
+        this.gitProperties = gitProperties;
+    }
+
     @GetMapping("/home")
     public String showHomePage(Model model) {
         String hostName = System.getenv("HOST_HOSTNAME");
         model.addAttribute("hostName", hostName);
+        model.addAttribute("commitId", gitProperties.getCommitId());
         return "home";
     }
 }
